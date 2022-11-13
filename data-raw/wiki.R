af0897471d09d71b29a10218030ad573a1b303c0
kapsner <lorenz.kapsner@web.de>
2022-11-13 09:05:24 +0100

    chore: updated wiki.r

--- data-raw/wiki.R
+++ data-raw/wiki.R
@@ -1,11 +1,20 @@
 # nolint start
 
 output_dir <- "../mllrnrs.wiki"
+url <- "https://github.com/kapsner/mllrnrs.wiki.git"
 
-git2r::clone(
-  url = "https://github.com/kapsner/mllrnrs.wiki.git",
-  local_path = output_dir
-)
+tryCatch({
+  git2r::clone(
+    url = url,
+    local_path = output_dir
+  )
+}, error = function(e) {
+  message(e)
+  git2r::pull(
+    repo = output_dir
+  )
+})
+
 
 file_list <- list.files(
   path = "vignettes",
