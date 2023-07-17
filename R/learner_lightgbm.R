7a9ef307926c07ca6f2b33c2d93f4431d2a0a8c8
kapsner <lorenz.kapsner@web.de>
2023-07-17 21:29:06 +0200

    fix: temporary fix for compatibility with lightgbm
    devel-version of lightgbm has differen api for predict method
    including different argument naming: newdata instead of data.
    a temporary fix has been added for temporary compatibility.
    this can be removed once lightgbm has been updated.

--- R/learner_lightgbm.R
+++ R/learner_lightgbm.R
@@ -281,11 +281,23 @@ lightgbm_predict <- function(model, newdata, ncores, ...) {
   kwargs <- list(...)
   args <- kdry::list.append(
     list(
-      object = model,
-      data = newdata # data in 3.3.2 and newdata in 3.3.2.99
+      object = model
+      # , data = newdata # data in 3.3.2 and newdata in 3.3.2.99
     ),
     kwargs
   )
+
+  # temporary fix due to API change of lightgbm's predict-method:
+  # - https://lightgbm.readthedocs.io/en/v3.3.5/R/reference/predict.lgb.Booster.html
+  # - https://lightgbm.readthedocs.io/en/v4.0.0/R/reference/predict.lgb.Booster.html
+  # this can be removed, once new version is out on cran, but then also
+  # add lightgbm>=4.0.0 as a requirement to the package description
+  if (utils::packageVersion("lightgbm") <= package_version("3.3.5")) {
+    args$data <- newdata
+  } else {
+    args$newdata <- newdata
+  }
+
   preds <- do.call(stats::predict, args)
   if (!is.null(kwargs$reshape)) {
     if (isTRUE(kwargs$reshape)) {
